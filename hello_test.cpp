bc5b19869709e6d10f1cbaebd0d72cd1f7fc51d0
Aleksei Skorodumov <aleksei.skorodumov@nozominetworks.com>
2021-10-13 21:09:20 +0200

    Add second test

--- hello_test.cpp
+++ hello_test.cpp
@@ -7,3 +7,10 @@ TEST(HelloTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
+
+TEST(HelloTest, second_test) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
